992f894fbf5bfc166cbb756b04286a30dcc3c899
mlea13 <mlea@princeton.edu>
2016-05-03 19:59:02 -0400

    distorted files

--- change_image.py
+++ change_image.py
@@ -2,21 +2,6 @@ import sys
 import cv2
 import numpy as np
 
-filename = sys.argv[1]
-pathname = sys.argv[2]
-im = cv2.imread(filename)
-height, width, _ = im.shape
-
-#smooth
-blur1 = cv2.blur(im,(25,25))
-blur2 = cv2.blur(im,(35,35))
-
-cv2.imwrite("data/" + pathname + "_b1.png", blur1)
-cv2.imwrite("data/" + pathname + "_b2.png", blur2)
-
-#color
-hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
-
 def change_hsv(column, img, amt):
     temp = img.copy()
     temp = temp.astype(float)
@@ -27,24 +12,13 @@ def change_hsv(column, img, amt):
     temp[high] = 255
     return cv2.cvtColor(temp.astype(np.uint8), cv2.COLOR_HSV2RGB)
 
-h1 = change_hsv(0, hsv, -70)
-h2 = change_hsv(0, hsv, -50)
-h3 = change_hsv(0, hsv, 50)
-h4 = change_hsv(0, hsv, 70)
-
-cv2.imwrite("data/" + pathname + "_h1.png", h1)
-cv2.imwrite("data/" + pathname + "_h2.png", h2)
-cv2.imwrite("data/" + pathname + "_h3.png", h3)
-cv2.imwrite("data/" + pathname + "_h4.png", h4)
-
-#warp
 def warp_color(a, w, img):
     temp = img.copy()
     A = temp.shape[0] / a
     W = w / temp.shape[1]
     shift = lambda x: A * np.sin(2.0*np.pi*x*W)
     
-    for i in range(temp.shape[0]):
+    for i in range(temp.shape[1]):
         temp[:,i] = np.roll(img[:,i], int(shift(i)))
     return temp
 
@@ -55,27 +29,55 @@ def warp(a, w, img):
     r_warp = warp_color(a, w, r)   
     return cv2.merge((b_warp,g_warp,r_warp))
 
-w1 = warp(3.0, 2.0, im)
-w2 = warp(6.0, 4.0, im)
-
-cv2.imwrite("data/" + pathname + "_w1.png",  w1)
-cv2.imwrite("data/" + pathname + "_w2.png",  w2)
-
-#add nois
 def noise(mean, var, sig, img):
     temp = img.copy()
+    height, width, _ = img.shape
     sigma = var**sig
     gauss = np.random.normal(mean, sigma, (height, width, 3))
     gauss = gauss.reshape(height, width, 3)
-    print gauss
-    print temp
     noise = temp + gauss
     return noise
 
-g1 = noise(0, 10, 1, im)
-g2 = noise(0, 30, 1, im)
-g3 = noise(0, 70, 1, im)
-
-cv2.imwrite("data/" + pathname + "_g1.png", g1)
-cv2.imwrite("data/" + pathname + "_g2.png", g2)
-cv2.imwrite("data/" + pathname + "_g3.png", g3)
+def distort_image(pathname, filename):
+    im = cv2.imread(filename)
+    height, width, _ = im.shape
+    
+    #smooth
+    blur1 = cv2.blur(im,(25,25))
+    blur2 = cv2.blur(im,(35,35))
+    blur3 = cv2.blur(im,(45,45))
+    
+    cv2.imwrite(pathname + "_b1.png", blur1)
+    cv2.imwrite(pathname + "_b2.png", blur2)
+    cv2.imwrite(pathname + "_b3.png", blur3)
+    
+    #color
+    hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
+    
+    h1 = change_hsv(0, hsv, -70)
+    h2 = change_hsv(0, hsv, -50)
+    h3 = change_hsv(0, hsv, 50)
+    h4 = change_hsv(0, hsv, 70)
+    
+    cv2.imwrite(pathname + "_h1.png", h1)
+    cv2.imwrite(pathname + "_h2.png", h2)
+    cv2.imwrite(pathname + "_h3.png", h3)
+    cv2.imwrite(pathname + "_h4.png", h4)
+    
+    #warp
+    w1 = warp(3.0, 2.0, im)
+    w2 = warp(6.0, 4.0, im)
+    w3 = warp(12.0, 8.0, im)
+    
+    cv2.imwrite(pathname + "_w1.png",  w1)
+    cv2.imwrite(pathname + "_w2.png",  w2)
+    cv2.imwrite(pathname + "_w3.png",  w3)
+    
+    #noise
+    n1 = noise(0, 10, 1, im)
+    n2 = noise(0, 30, 1, im)
+    n3 = noise(0, 70, 1, im)
+    
+    cv2.imwrite(pathname + "_n1.png", n1)
+    cv2.imwrite(pathname + "_n2.png", n2)
+    cv2.imwrite(pathname + "_n3.png", n3)
---
+++ distort_all_images.py
@@ -0,0 +1,18 @@
+import glob
+import sys
+import os
+import change_image as ci
+
+pathname = sys.argv[1]
+
+#cycle through all images in all directories
+top_dirs = glob.glob(pathname+ "*/")
+for top_dir in top_dirs:
+    dirs = glob.glob(top_dir + "*/")
+    for a_dir in dirs:
+        imgs = glob.glob(a_dir+ "*.jpg")
+        for im in imgs:
+            im_number = im.split("/")[-1].split(".")[0]
+            new_path = a_dir + im_number + "/" + im_number
+            print new_path
+            ci.distort_image(new_path, im)
