d1c8a61ad8a1c6f6ae307ecbbcc312d6acc15276
mlea13 <mlea@princeton.edu>
2016-05-03 20:17:10 -0400

    new distort

--- change_image.py
+++ change_image.py
@@ -43,9 +43,9 @@ def distort_image(pathname, filename):
     height, width, _ = im.shape
     
     #smooth
-    blur1 = cv2.blur(im,(25,25))
-    blur2 = cv2.blur(im,(35,35))
-    blur3 = cv2.blur(im,(45,45))
+    blur1 = cv2.blur(im,(10,10))
+    blur2 = cv2.blur(im,(30,30))
+    blur3 = cv2.blur(im,(50,50))
     
     cv2.imwrite(pathname + "_b1.png", blur1)
     cv2.imwrite(pathname + "_b2.png", blur2)
@@ -55,8 +55,8 @@ def distort_image(pathname, filename):
     hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
     
     h1 = change_hsv(0, hsv, -70)
-    h2 = change_hsv(0, hsv, -50)
-    h3 = change_hsv(0, hsv, 50)
+    h2 = change_hsv(0, hsv, -20)
+    h3 = change_hsv(0, hsv, 20)
     h4 = change_hsv(0, hsv, 70)
     
     cv2.imwrite(pathname + "_h1.png", h1)
@@ -65,18 +65,18 @@ def distort_image(pathname, filename):
     cv2.imwrite(pathname + "_h4.png", h4)
     
     #warp
-    w1 = warp(3.0, 2.0, im)
-    w2 = warp(6.0, 4.0, im)
-    w3 = warp(12.0, 8.0, im)
+    w1 = warp(30, 30, im)
+    w2 = warp(20, 20, im)
+    w3 = warp(10, 10, im)
     
     cv2.imwrite(pathname + "_w1.png",  w1)
     cv2.imwrite(pathname + "_w2.png",  w2)
     cv2.imwrite(pathname + "_w3.png",  w3)
     
     #noise
-    n1 = noise(0, 10, 1, im)
-    n2 = noise(0, 30, 1, im)
-    n3 = noise(0, 70, 1, im)
+    n1 = noise(0, 30, 1, im)
+    n2 = noise(0, 70, 1, im)
+    n3 = noise(0, 120, 1, im)
     
     cv2.imwrite(pathname + "_n1.png", n1)
     cv2.imwrite(pathname + "_n2.png", n2)
